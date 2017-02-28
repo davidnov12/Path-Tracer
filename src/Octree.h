4773979679366b0855e245a2d92614019cf1a69f
davidnov12 <nvdavi2@gmail.com>
2017-02-28 23:50:28 +0100

    Update Octree

--- src/Octree.h
+++ src/Octree.h
@@ -1,11 +1,11 @@
 /*
- * Path tracing na GPU
- * Bakalarska prace
- * David Novak, xnovak1l
- * FIT VUT Brno, 2016
- *
- * Octree.h - trida pro praci s oktalovym stromem
- */
+* Path tracing na GPU
+* Bakalarska prace
+* David Novak, xnovak1l
+* FIT VUT Brno, 2016
+*
+* Octree.h - trida pro praci s oktalovym stromem
+*/
 
 #pragma once
 
@@ -14,8 +14,11 @@
 #define LEFT -0.7
 #define RIGHT 0.7
 #define UP 0.7
-#define DOWN -0.52
+#define DOWN -0.5
 #define FRONT -0.9
+#define BACK 0.9
+
+#define MAX_NODES 15 * md.triangles_count
 
 class Octree {
 
@@ -26,20 +29,34 @@ public:
 		int index;
 		int count;
 		int leaf;
+		int gap;
 		vec4 start;
 		vec4 end;
 	} Node;
 
 	Octree(Scene::Model md, int primitivesPerList);
+	int* getIndices();
+	Node* getNodes();
+	int getIndicesLength();
+	int getNodesCount();
 
-private:
+	//private:
 
 	void initOctree();
 	void buildOctree(Scene::Model md, int primitivesPerList);
 	bool isInBox(vec3 point, vec3 startBox, vec3 endBox);
-	void makeChilds(Node n);
-	bool edgeBoxIntersection(vec4 origin, vec4 edge, vec3 boxStart, vec3 boxEnd, float length);
+	void makeChilds(int index);
+	bool edgeBoxIntersection(vec4 origin, vec4 edge, vec3 boxStart, vec3 boxEnd);
 	bool uniqueIndices(int inserted, int nodeIndex);
+	void linkOctree();
+
+	void printTree();
+	void printVec(vec3 v);
+	void octreeTraversal(vec3 origin, vec3 dir);
+	void iterative(int node);
+	void printUsesLeafs();
+	int first_node(double tx0, double ty0, double tz0, double txm, double tym, double tzm);
+	int new_node(double txm, int x, double tym, int y, double tzm, int z);
 
 	Node* root;
 	vector<Node> nodes;
@@ -47,6 +64,7 @@ private:
 	vector<int> indices;
 	int nodes_count;
 
-	float t;
+	float t = 5.2;
+	float tx0, ty0, tz0, tx1, ty1, tz1;
 
 };
